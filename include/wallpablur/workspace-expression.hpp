864d6f5d801efed37e22b6d2b743733b7d484f4d
wolmibo <102993087+wolmibo@users.noreply.github.com>
2023-04-08 13:39:32 +0200

    Add (unused) workspace-expression

---
+++ include/wallpablur/workspace-expression.hpp
@@ -0,0 +1,62 @@
+#ifndef WALLPABLUR_WORKSPACE_EXPRESSION_HPP_INCLUDED
+#define WALLPABLUR_WORKSPACE_EXPRESSION_HPP_INCLUDED
+
+#include "wallpablur/expression/boolean.hpp"
+#include "wallpablur/expression/string-compare.hpp"
+#include "wallpablur/workspace.hpp"
+
+#include <optional>
+#include <string>
+#include <variant>
+
+#include <iconfigp/value-parser.hpp>
+
+
+
+class workspace_expression_condition {
+  public:
+    enum class string_var {
+      name,
+      output
+    };
+
+    using string_expr = std::pair<expression::string_compare, string_var>;
+
+
+
+    [[nodiscard]] static std::optional<workspace_expression_condition>
+      from_string(std::string_view);
+
+
+
+    [[nodiscard]] bool evaluate(const workspace& ws) const;
+
+
+
+  private:
+    std::variant<
+      string_expr
+    > cond_;
+
+
+
+    template<typename...Args>
+    workspace_expression_condition(Args&&... args) :
+      cond_{std::forward<Args>(args)...}
+    {}
+};
+
+
+
+using workspace_expression = expression::boolean<workspace_expression_condition>;
+
+
+
+template<> struct iconfigp::value_parser<workspace_expression> {
+  static constexpr std::string_view name {"workspace expression"};
+
+  static std::string_view                    format();
+  static std::optional<workspace_expression> parse(std::string_view);
+};
+
+#endif // WALLPABLUR_WORKSPACE_EXPRESSION_HPP_INCLUDED
---
+++ src/workspace-expression.cpp
@@ -0,0 +1,94 @@
+#include "wallpablur/expression/parser.hpp"
+#include "wallpablur/expression/string-compare.hpp"
+#include "wallpablur/workspace-expression.hpp"
+
+
+
+bool workspace_expression_condition::evaluate(const workspace& ws) const {
+  if (std::holds_alternative<string_expr>(cond_)) {
+    const auto& [expr, var] = std::get<string_expr>(cond_);
+
+    switch (var) {
+      case string_var::name:   return expr.evaluate(ws.name());
+      case string_var::output: return expr.evaluate(ws.output());
+    }
+
+    return false;
+  }
+
+
+
+  std::unreachable();
+}
+
+
+
+
+
+std::string_view iconfigp::value_parser<workspace_expression>::format() {
+  return
+    "Boolean expression formed from the terms:\n"
+    "  (ws_name|output) == [*]<string>[*]\n"
+    "which can be combined (in descending precendence) using:\n"
+    "  (), ! (prefix), && (infix), and || (infix)\n";
+}
+
+
+
+template<>
+struct iconfigp::case_insensitive_parse_lut<workspace_expression_condition::string_var> {
+  static constexpr std::string_view name {"surface-variable"};
+  static constexpr std::array<std::pair<std::string_view,
+                                      workspace_expression_condition::string_var>, 3> lut
+  {
+    std::make_pair("ws_name", workspace_expression_condition::string_var::name),
+    std::make_pair("ws-name", workspace_expression_condition::string_var::name),
+    std::make_pair("output",  workspace_expression_condition::string_var::output),
+  };
+};
+
+
+
+
+namespace {
+  [[nodiscard]] std::optional<workspace_expression_condition::string_expr> parse_str_expr(
+      std::string_view input
+  ) {
+    auto res = expression::parse_string_compare(input);
+    if (!res) {
+      return {};
+    }
+
+    auto& [expr, var_str] = *res;
+
+    auto var = iconfigp::value_parser<workspace_expression_condition::string_var>
+                       ::parse(var_str);
+    if (!var) {
+      return {};
+    }
+
+    return std::make_pair(std::move(expr), *var);
+  }
+}
+
+
+
+std::optional<workspace_expression_condition> workspace_expression_condition::from_string(
+    std::string_view in
+) {
+  if (auto str = parse_str_expr(in)) {
+    return workspace_expression_condition{std::move(*str)};
+  }
+
+  return {};
+}
+
+
+
+
+std::optional<workspace_expression> iconfigp::value_parser<workspace_expression>::parse(
+    std::string_view input
+) {
+  return expression::parser<workspace_expression_condition>::parse(input);
+}
+
